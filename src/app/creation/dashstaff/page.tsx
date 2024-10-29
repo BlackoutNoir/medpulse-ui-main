67a71ffae6bc9088a4f49747561001dbf183343a
BlackoutO <blackoutdevs@gmail.com>
2024-10-29 13:35:54 +0300

    feat(dashboard-users): stat-cards as separate components

--- src/app/creation/dashstaff/page.tsx
+++ src/app/creation/dashstaff/page.tsx
@@ -7,14 +7,14 @@ import { DoctorOverview } from '@/features/dashboard/doctors/overview';
 import { PatientOverview } from '@/features/dashboard/patients/overview';
 import { Search } from '@/features/dashboard/components/search';
 import TeamSwitcher from '@/features/dashboard/components/team-switcher';
-//import { UpperStats } from '@/features/dashboard/components/upper-stats';
 import { UserNav } from '@/features/dashboard/components/user-nav';
+import { DoctorsStatCards } from '@/features/dashboard/doctors/stat-cards';
+import { PatientsStatCards } from '@/features/dashboard/patients/stat-cards';
 
 export default async function CreationPage() {
-
   return (
     <>
-      <div className="flex-col md:flex"> {/*hidden*/}
+      <div className="flex-col md:flex">
         <div className="border-b">
           <div className="hidden md:flex h-16 items-center px-4">
             <TeamSwitcher />
@@ -38,108 +38,32 @@ export default async function CreationPage() {
 
           <Tabs>
             <TabsList>
-              <TabsTrigger value="overview">Overview</TabsTrigger>
-              <TabsTrigger value="all">All</TabsTrigger>
+              <TabsTrigger value="users">All</TabsTrigger>
+              <TabsTrigger value="doctors">Doctors</TabsTrigger>
               <TabsTrigger value="patients">Patients</TabsTrigger>
-              <TabsTrigger value="full-time">Full-Time</TabsTrigger>
+              <TabsTrigger value="staff">Staff</TabsTrigger>
             </TabsList>
-            <TabsContent value="overview" className="space-y-4">
-              <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
-                <Card>
-                  <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                    <CardTitle className="text-sm font-medium">Total Revenue</CardTitle>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      className="h-4 w-4 text-muted-foreground"
-                    >
-                      <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
-                    </svg>
-                  </CardHeader>
-                  <CardContent>
-                    <div className="text-2xl font-bold">$45,231.89</div>
-                    <p className="text-xs text-muted-foreground">+20.1% from last month</p>
-                  </CardContent>
-                </Card>
-                <Card>
-                  <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                    <CardTitle className="text-sm font-medium">Subscriptions</CardTitle>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      className="h-4 w-4 text-muted-foreground"
-                    >
-                      <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
-                      <circle cx="9" cy="7" r="4" />
-                      <path d="M22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75" />
-                    </svg>
-                  </CardHeader>
-                  <CardContent>
-                    <div className="text-2xl font-bold">+2350</div>
-                    <p className="text-xs text-muted-foreground">+180.1% from last month</p>
-                  </CardContent>
-                </Card>
-                <Card>
-                  <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                    <CardTitle className="text-sm font-medium">Sales</CardTitle>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      className="h-4 w-4 text-muted-foreground"
-                    >
-                      <rect width="20" height="14" x="2" y="5" rx="2" />
-                      <path d="M2 10h20" />
-                    </svg>
-                  </CardHeader>
-                  <CardContent>
-                    <div className="text-2xl font-bold">+12,234</div>
-                    <p className="text-xs text-muted-foreground">+19% from last month</p>
-                  </CardContent>
-                </Card>
-                <Card>
-                  <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                    <CardTitle className="text-sm font-medium">Active Now</CardTitle>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      className="h-4 w-4 text-muted-foreground"
-                    >
-                      <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
-                    </svg>
-                  </CardHeader>
-                  <CardContent>
-                    <div className="text-2xl font-bold">+573</div>
-                    <p className="text-xs text-muted-foreground">+201 since last hour</p>
-                  </CardContent>
-                </Card>
-              </div>
+            <TabsContent value="users" className="space-y-4">
+              <DoctorsStatCards />
               <div className="">
                 <Card className="">
                   <CardHeader>
                     <CardTitle>Overview</CardTitle>
-                    <CardDescription>
-                      See list of doctors.
-                    </CardDescription>
+                    <CardDescription>See list of Users.</CardDescription>
+                  </CardHeader>
+                  <CardContent className="mx-auto">
+                    <DoctorOverview />
+                  </CardContent>
+                </Card>
+              </div>
+            </TabsContent>
+            <TabsContent value="doctors" className="space-y-4">
+              <DoctorsStatCards />
+              <div className="">
+                <Card className="">
+                  <CardHeader>
+                    <CardTitle>Overview</CardTitle>
+                    <CardDescription>See list of Doctors.</CardDescription>
                   </CardHeader>
                   <CardContent className="mx-auto">
                     <DoctorOverview />
@@ -148,102 +72,12 @@ export default async function CreationPage() {
               </div>
             </TabsContent>
             <TabsContent value="patients" className="space-y-4">
-              <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
-                <Card>
-                  <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                    <CardTitle className="text-sm font-medium">Total Revenue</CardTitle>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      className="h-4 w-4 text-muted-foreground"
-                    >
-                      <path d="M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6" />
-                    </svg>
-                  </CardHeader>
-                  <CardContent>
-                    <div className="text-2xl font-bold">$45,231.89</div>
-                    <p className="text-xs text-muted-foreground">+20.1% from last month</p>
-                  </CardContent>
-                </Card>
-                <Card>
-                  <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                    <CardTitle className="text-sm font-medium">Subscriptions</CardTitle>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      className="h-4 w-4 text-muted-foreground"
-                    >
-                      <path d="M16 21v-2a4 4 0 0 0-4-4H6a4 4 0 0 0-4 4v2" />
-                      <circle cx="9" cy="7" r="4" />
-                      <path d="M22 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75" />
-                    </svg>
-                  </CardHeader>
-                  <CardContent>
-                    <div className="text-2xl font-bold">+2350</div>
-                    <p className="text-xs text-muted-foreground">+180.1% from last month</p>
-                  </CardContent>
-                </Card>
-                <Card>
-                  <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                    <CardTitle className="text-sm font-medium">Sales</CardTitle>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      className="h-4 w-4 text-muted-foreground"
-                    >
-                      <rect width="20" height="14" x="2" y="5" rx="2" />
-                      <path d="M2 10h20" />
-                    </svg>
-                  </CardHeader>
-                  <CardContent>
-                    <div className="text-2xl font-bold">+12,234</div>
-                    <p className="text-xs text-muted-foreground">+19% from last month</p>
-                  </CardContent>
-                </Card>
-                <Card>
-                  <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-                    <CardTitle className="text-sm font-medium">Active Now</CardTitle>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      viewBox="0 0 24 24"
-                      fill="none"
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      className="h-4 w-4 text-muted-foreground"
-                    >
-                      <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
-                    </svg>
-                  </CardHeader>
-                  <CardContent>
-                    <div className="text-2xl font-bold">+573</div>
-                    <p className="text-xs text-muted-foreground">+201 since last hour</p>
-                  </CardContent>
-                </Card>
-              </div>
+              <PatientsStatCards />
               <div className="">
                 <Card className="">
                   <CardHeader>
                     <CardTitle>Overview</CardTitle>
-                    <CardDescription>
-                      See list of patients.
-                    </CardDescription>
+                    <CardDescription>See list of Patients.</CardDescription>
                   </CardHeader>
                   <CardContent className="mx-auto">
                     <PatientOverview />
@@ -258,8 +92,6 @@ export default async function CreationPage() {
   );
 }
 
-
-
 /*
 CSS (JSMastery Health platform):
   .data-table {
@@ -270,22 +102,4 @@ CSS (JSMastery Health platform):
     @apply flex w-full items-center justify-between space-x-2 p-4;
   }
 
-
-*/
-
-/*
-<div className="">
-  <div>
-    <h2 className="text-3xl font-bold tracking-tight">Doctors List</h2>
-    <p className="text-muted-foreground">
-      Here&apos;s the list of doctors for your organization!
-    </p>
-  </div>
-  <div className="">
-    <UpperStats />
-  </div>
-  <div className="">
-    <DataTable columns={columns} data={data} />
-  </div>
-</div> 
 */
