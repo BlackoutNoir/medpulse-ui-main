0b6288833bd741983af8031917c4476815a3daeb
AAlghoul9 <aa2103693@qu.edu.qa>
2024-11-17 18:05:16 +0300

    appointmetns tab

--- src/app/manageMedicalPrescription/page.tsx
+++ src/app/manageMedicalPrescription/page.tsx
@@ -24,13 +24,11 @@ export default async function ManagePatientPage() {
           <div className="flex items-center justify-between space-y-2">
             <div>
               <Avatar className="w-16 h-16">
-                <AvatarImage src={"https://github.com/shadcn.png"} alt={"John Doe"} />
-                <AvatarFallback>{"JD"}</AvatarFallback>
-                </Avatar>
+                <AvatarImage src={'https://github.com/shadcn.png'} alt={'John Doe'} />
+                <AvatarFallback>{'JD'}</AvatarFallback>
+              </Avatar>
               <h2 className="text-3xl font-bold tracking-tight">My Profile</h2>
-              <p className="text-muted-foreground">
-                @JohnDoe
-              </p>
+              <p className="text-muted-foreground">@JohnDoe</p>
             </div>
           </div>
 
@@ -41,6 +39,7 @@ export default async function ManagePatientPage() {
               <TabsTrigger value="medical-record">Medical Record</TabsTrigger>
               <TabsTrigger value="lab-results">Lab Results</TabsTrigger>
               <TabsTrigger value="current-prescriptions">Current Prescriptions</TabsTrigger>
+              <TabsTrigger value="appointments">Appointments</TabsTrigger>
             </TabsList>
             <TabsContent value="patient-info" className="space-y-4">
               <div className="">
@@ -98,6 +97,19 @@ export default async function ManagePatientPage() {
               </div>
             </TabsContent>
             <TabsContent value="current-prescriptions" className="space-y-4">
+              <div className="">
+                <Card className="">
+                  <CardHeader>
+                    <CardTitle>Current Prescriptions</CardTitle>
+                    <CardDescription>View your currently prescribed medicine.</CardDescription>
+                  </CardHeader>
+                  <CardContent className="mx-auto">
+                    <PatientOverview />
+                  </CardContent>
+                </Card>
+              </div>
+            </TabsContent>
+            <TabsContent value="appointments" className="space-y-4">
               <div className="">
                 <Card className="">
                   <CardHeader>
